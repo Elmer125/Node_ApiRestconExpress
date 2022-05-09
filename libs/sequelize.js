6a024ef5cc47602ccc17c9328c193ae86a0179de
Elmer Mauricio <86616638+Elmer125@users.noreply.github.com>
2022-05-09 10:37:24 -0500

    fix error

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,23 +2,25 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(config.dbUser);
+/* const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`; */
 /* const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`; */
 
-const option = {
+const options = {
   dialect: 'postgres',
   logging: config.isProd ? false : true,
-};
-
-if (config.isProd) {
-  option.ssl = {
-    rejectUnauthorized: false,
-  };
 }
 
-const sequelize = new Sequelize(URI, option);
+if (config.isProd) {
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
+
+const sequelize = new Sequelize(config.dburl, options);
 
 //PARA UTILIZARLO NORMAL SIN HEROKU
 /* const sequelize = new Sequelize(URI, {
